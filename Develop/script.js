533c5857dcbbe4d4a2017c78e05d688a8853d1b4
Ricardo Russ <ricardoruss@live.com>
2020-03-06 18:49:29 -0500

    fixed basic password generator functionality, now need to insure selects from all sets and creates string

--- Develop/script.js
+++ Develop/script.js
@@ -118,28 +118,14 @@ var upperCaseCharacters = [
 ];
 // Defines uppercase characters.
 
-var allConfirm = [
-  upperCaseCharacters +
-    lowerCaseCharacters +
-    numberCharacters +
-    specialCharacters
-];
-var SNLc = [lowerCaseCharacters + numberCharacters + specialCharacters];
-var SNUc = [upperCaseCharacters + numberCharacters + specialCharacters];
-var SLcUc = [upperCaseCharacters + lowerCaseCharacters + specialCharacters];
-var NLcUc = [numberCharacters + lowerCaseCharacters + specialCharacters];
-var SN = [numberCharacters + specialCharacters];
-var SLc = [lowerCaseCharacters + specialCharacters];
-var SUc = [specialCharacters + upperCaseCharacters];
-var NLc = [numberCharacters + lowerCaseCharacters];
-var Nuc = [numberCharacters + upperCaseCharacters];
-var LcUc = [lowerCaseCharacters + upperCaseCharacters];
+
 
 var passLength = parseInt(
   prompt(
     "How long do you want your password to be? (must be an integer between 8 and 128)"
   )
 );
+console.log(passLength)
 // Defines password length
 
 // Generate Password
@@ -154,49 +140,36 @@ var passLength = parseInt(
 
 // Figure out how to deny access or reload page if password does not meet parameters
 
-
 function generatePassword() {
-  // for (var i = 0; i <= passLength; i++) {
-    
-  const matchedParameterValues = []
-  if ((upperCaseCharactersQuery) matchedParameterValues = matchedParameterValues.concat(upperCaseCharacters))
-  if ((lowerCaseCharactersQuery) matchedParameterValues = matchedParameterValues.concat(lowerCaseCharacters))
-  if ((specialCharactersQuery) matchedParameterValues = matchedParameterValues.concat(specialCharacters))
-  if ((numberCharactersQuery) matchedParameterValues = matchedParameterValues.concat(numberCharacters))
+
+  var matchedParameterValues = []; 
+  
+  if (numberCharactersQuery = true) matchedParameterValues = matchedParameterValues.concat(numberCharacters);
+  if (upperCaseCharactersQuery = true) matchedParameterValues = matchedParameterValues.concat(upperCaseCharacters);
+  if (lowerCaseCharactersQuery = true) matchedParameterValues = matchedParameterValues.concat(lowerCaseCharacters);
+  if (specialCharactersQuery = true) matchedParameterValues = matchedParameterValues.concat(specialCharacters);
+  
+  
+
+      
   if (passLength < 8 || passLength > 128) {
-    alert("Follow password length instructions please!");
-  } else if (
-    (upperCaseCharactersQuery &&
-      lowerCaseCharactersQuery &&
-      numberCharactersQuery &&
-      specialCharactersQuery) === true
-  // ) {(for (var i = 0; i <= passLength; i++) {
-    
-    var actualPass =
-    return actualPass
-        
-    );
-  } else if (
-    (lowerCaseCharactersQuery &&
-      numberCharactersQuery &&
-      specialCharactersQuery) === true
-  ) {
-    return "your password is " + SNLc[Math.floor(Math.random() * SNLc.length)];
-  } else {
-    return "Please select all categories for a secure password";
+    alert("Follow password length instructions please! To get a new password, refresh the page");
+  } 
+  else{
+   for (var i = 0; i <= passLength; i++){
+    var indexNum = (Math.floor(Math.random() * passLength))
+    return "your password is " +  matchedParameterValues[indexNum];}
+   }
   }
-}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Current errors:
-// Need to figure out how to select random elements of the array, instead of just... random array. for loop?
-// Code is not DRY compliant. 
