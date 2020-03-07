452067ff0ef3dbd39e72c73d7b38447abf5cadd7
Ricardo Russ <ricardoruss@live.com>
2020-03-06 20:53:06 -0500

    Fixed length of password function, now have to write Readme

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ alert("You need a new password");
 
 var specialCharactersQuery = confirm("Do you want special characters?");
 //Confirms if user wants special characters in their password,
-// if yes includes, if no does not. Shorthand is S
+// if yes includes, if no does not.
 
 var specialCharacters = [
   "!",
@@ -45,14 +45,14 @@ var specialCharacters = [
 
 var numberCharactersQuery = confirm("Do you want numbers?");
 // Confirms if user wants numbers in their password,
-// if yes includes, if no does not. Shorthand is N
+// if yes includes, if no does not.
 
 var numberCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 // Defines numbers for use in password
 
 var lowerCaseCharactersQuery = confirm("Do you want lower case characters?");
 // // Confirms if user wants lowercase characters in their password, if yes includes,
-// if no does not. Shorthand is Lc
+// if no does not.
 
 var lowerCaseCharacters = [
   "a",
@@ -86,7 +86,7 @@ var lowerCaseCharacters = [
 
 var upperCaseCharactersQuery = confirm("Do you want upper case characters?");
 // Confirms if user wants uppercase characters in their password,
-// if yes includes, if no does not. Shorthand is Uc
+// if yes includes, if no does not.
 
 var upperCaseCharacters = [
   "A",
@@ -118,62 +118,60 @@ var upperCaseCharacters = [
 ];
 // Defines uppercase characters.
 
-
-
 var passLength = parseInt(
   prompt(
     "How long do you want your password to be? (must be an integer between 8 and 128)"
   )
 );
-console.log(passLength);
-// Defines password length
-
-// Generate Password
-// prompt user for password length
-// check to see if the user actually entered a length.
-// 1. special characters,
-// 2. numbers
-// 3. lower case
-// 4.uppercase
-// generate password based on user specifications:
-// Note: user specifications come from confirms. (save in variables)
 
 var actualPass = [];
+// Variable that returns eventual password.
 
+// Function that generates password according to parameters selected.
 function generatePassword() {
+  for (var i = 0; i <= passLength; i++) {
+    var matchedParameterValues = [];
+    for (var i = 0; i <= passLength; i++) {
+      if (numberCharactersQuery)
+        matchedParameterValues = matchedParameterValues.concat(
+          numberCharacters
+        );
+      if (upperCaseCharactersQuery)
+        matchedParameterValues = matchedParameterValues.concat(
+          upperCaseCharacters
+        );
+      if (lowerCaseCharactersQuery)
+        matchedParameterValues = matchedParameterValues.concat(
+          lowerCaseCharacters
+        );
+      if (specialCharactersQuery)
+        matchedParameterValues = matchedParameterValues.concat(
+          specialCharacters
+        );
+    }
 
-  var matchedParameterValues = []; 
-
-  if (numberCharactersQuery) matchedParameterValues = matchedParameterValues.concat(numberCharacters);
-  if (upperCaseCharactersQuery) matchedParameterValues = matchedParameterValues.concat(upperCaseCharacters);
-  if (lowerCaseCharactersQuery) matchedParameterValues = matchedParameterValues.concat(lowerCaseCharacters);
-  if (specialCharactersQuery) matchedParameterValues = matchedParameterValues.concat(specialCharacters);
-  
-      
-  if (passLength < 8 || passLength > 128) {
-    alert("Follow password length instructions please! To get a new password, refresh the page");
-  } 
-  else{
-   for (var i = 0; i <= passLength; i++){
-     Math.floor(Math.random() + i);
-     for (var j = 0; j <= passLength; j++){
-      var indexNum = (Math.floor(Math.random() * 10 + 1));
-     }
-    var Pass2 = matchedParameterValues[indexNum]
-    actualPass.push(Pass2)
-    console.log(actualPass.join(""));
-     return ("your password is " + actualPass.join(""));
+    if (passLength < 8 || passLength > 128) {
+      alert(
+        "Follow password length instructions please! To get a new password, refresh the page"
+      );
+    } else {
+      for (var i = 0; i < passLength; i++) {
+        for (var j = 0; j < passLength; j++) {
+          var indexNum = Math.floor(Math.random() * 10 + 1);
+        }
+        var Pass2 = matchedParameterValues[indexNum];
+        actualPass.push(Pass2);
+      }
+      return "your password is " + actualPass.join("");
+    }
   }
-   }
-  }
-
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value = "your password is " + actualPass.join("");
 }
 
 // Add event listener to generate button
